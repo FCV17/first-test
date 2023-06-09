5a911126395a323e06fdd8f486654cb2fcaf9e7d
filipeveiga@FCVMac <filipe.veiga@protonmail.com>
2023-06-09 23:28:35 +0100

    Initial Commit

---
+++ single-app-test-git/single_app_test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  single_app_test_gitApp.swift
+//  single-app-test-git
+//
+//  Created by Filipe Veiga on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct single_app_test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ single-app-test-gitUITests/single_app_test_gitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  single_app_test_gitUITestsLaunchTests.swift
+//  single-app-test-gitUITests
+//
+//  Created by Filipe Veiga on 09/06/2023.
+//
+
+import XCTest
+
+final class single_app_test_gitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
